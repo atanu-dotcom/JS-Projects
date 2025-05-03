17782f6167a6679e3a352d09a6c0aaa845712de3
Atanu Bhunia <cse21122@cemk.ac.in>
2025-05-03 10:06:02 +0530

    Animated Search Bar

---
+++ 7. SearchBar/script.js
@@ -0,0 +1,6 @@
+const search = document.querySelector('.search-bar-container');
+const magnifier = document.querySelector('.magnifier');
+
+magnifier.addEventListener('click', () => {
+    search.classList.toggle('active');
+});
